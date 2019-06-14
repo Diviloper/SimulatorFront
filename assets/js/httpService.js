8b2e04e9c7bc39de464d437da67687298207b517
Ineskay <victordivi98@gmail.com>
2019-06-14 17:10:11 +0200

    Add id to get

--- assets/js/httpService.js
+++ assets/js/httpService.js
@@ -1,15 +1,3 @@
-function getQueueTrace() {
-    $.ajax({
-        url: 'http://localhost:5000/queueTrace',
-        method: 'GET',
-        success: function (response) {
-            console.log(response);
-            return response;
-        },
-        dataType: 'json',
-    });
-}
-
 function changeParameters() {
     const formData = {
         'n_cranes': document.getElementById('n_cranes').value,
@@ -28,8 +16,7 @@ function changeParameters() {
         url: 'http://localhost:5000/post',
         method: 'POST',
         success: function (response) {
-            console.log(response);
-            return response;
+            window.localStorage.setItem('simulation_id', response['simulation_id'])
         },
         data: JSON.stringify(formData),
         dataType: 'json',
@@ -37,90 +24,96 @@ function changeParameters() {
 }
 
 function getResults() {
-    $.ajax({
-        url: 'http://localhost:5000/results',
-        method: 'GET',
-        success: function (response) {
-            console.log(response);
+    let id = window.localStorage.getItem('simulation_id');
+    if (id === null) {
+        changeParameters();
+        while(id === null) id = window.localStorage.getItem('simulation_id');
+    } else {
+        $.ajax({
+            url: `http://localhost:5000/results/${id}`,
+            method: 'GET',
+            success: function (response) {
+                console.log(response);
 
-            document.getElementById('mean_time').innerText = response['mean_time'] + 's';
-            document.getElementById('queue_percentage').innerText = response['queue_percentage'] + '%';
-            document.getElementById('max_time').innerText = response['max_time'] + 's';
+                document.getElementById('mean_time').innerText = response['mean_time'] + 's';
+                document.getElementById('queue_percentage').innerText = response['queue_percentage'] + '%';
+                document.getElementById('max_time').innerText = response['max_time'] + 's';
 
-            ctx = document.getElementById('chartHours').getContext("2d");
-            matrix = response['queue_matrix'];
-            matrix = aggregate(matrix);
+                ctx = document.getElementById('chartHours').getContext("2d");
+                matrix = response['queue_matrix'];
+                matrix = aggregate(matrix);
 
-            datasets = [];
-            for(let i=0; i<matrix.size(); ++i){
-                datasets.add({
-                    borderColor: getColor(i),
-                    backgroundColor: getColor(i),
-                    pointRadius: 0,
-                    pointHoverRadius: 0,
-                    borderWidth: 3,
-                    data: matrix[i]
-                });
-            }
-
-            myChart = new Chart(ctx, {
-                type: 'line',
-
-                data: {
-                    labels: ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", " 13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"],
-                    datasets: datasets
-                },
-                options: {
-                    legend: {
-                        display: false
-                    },
-
-                    tooltips: {
-                        enabled: false
-                    },
-
-                    scales: {
-                        yAxes: [{
-
-                            ticks: {
-                                fontColor: "#9f9f9f",
-                                beginAtZero: false,
-                                maxTicksLimit: 5,
-                                //padding: 20
-                            },
-                            gridLines: {
-                                drawBorder: false,
-                                zeroLineColor: "#ccc",
-                                color: 'rgba(255,255,255,0.05)'
-                            }
-
-                        }],
-
-                        xAxes: [{
-                            barPercentage: 1.6,
-                            gridLines: {
-                                drawBorder: false,
-                                color: 'rgba(255,255,255,0.1)',
-                                zeroLineColor: "transparent",
-                                display: false,
-                            },
-                            ticks: {
-                                padding: 20,
-                                fontColor: "#9f9f9f"
-                            }
-                        }]
-                    },
+                datasets = [];
+                for (let i = 0; i < matrix.size(); ++i) {
+                    datasets.add({
+                        borderColor: getColor(i),
+                        backgroundColor: getColor(i),
+                        pointRadius: 0,
+                        pointHoverRadius: 0,
+                        borderWidth: 3,
+                        data: matrix[i]
+                    });
                 }
-            });
-        },
-        dataType: 'json',
-    });
+
+                myChart = new Chart(ctx, {
+                    type: 'line',
+
+                    data: {
+                        labels: ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", " 13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"],
+                        datasets: datasets
+                    },
+                    options: {
+                        legend: {
+                            display: false
+                        },
+
+                        tooltips: {
+                            enabled: false
+                        },
+
+                        scales: {
+                            yAxes: [{
+
+                                ticks: {
+                                    fontColor: "#9f9f9f",
+                                    beginAtZero: false,
+                                    maxTicksLimit: 5,
+                                    //padding: 20
+                                },
+                                gridLines: {
+                                    drawBorder: false,
+                                    zeroLineColor: "#ccc",
+                                    color: 'rgba(255,255,255,0.05)'
+                                }
+
+                            }],
+
+                            xAxes: [{
+                                barPercentage: 1.6,
+                                gridLines: {
+                                    drawBorder: false,
+                                    color: 'rgba(255,255,255,0.1)',
+                                    zeroLineColor: "transparent",
+                                    display: false,
+                                },
+                                ticks: {
+                                    padding: 20,
+                                    fontColor: "#9f9f9f"
+                                }
+                            }]
+                        },
+                    }
+                });
+            },
+            dataType: 'json',
+        });
+    }
 }
 
 function aggregate(matrix) {
-    for(let i=1; i<matrix.size(); ++i){
-        for(let j=0; j<24; ++j){
-            matrix[i][j] += matrix[i-1][j];
+    for (let i = 1; i < matrix.size(); ++i) {
+        for (let j = 0; j < 24; ++j) {
+            matrix[i][j] += matrix[i - 1][j];
         }
     }
     return matrix;
@@ -138,3 +131,4 @@ function getColor(i) {
             return "#51CBCE";
     }
 }
+
